7df3f1462a5ef90a95353dc4c83f0fe7e907ff1a
OSemilore <oluwatosinigbayilola@gmail.com>
2022-04-14 11:44:46 -0700

    variadic_functions.h

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,26 +11,25 @@ int _atoi(char *s)
 
 	while (*(s + count) != '\0')
 	{
-		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+		if (size > 0 && (*(s + count) > '9'))
 			break;
 
-		if (*(s + count) == '-')								
+		if (*(s + count) == '-')
 			pn *= -1;
-
-		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		if ((*s + count) >= '0') && (*(s + count) <= '9'))
 		{
-			if (size > 0)
-				m *= 10;
-			size++;
+		      if (size > 0)
+		              m*= 10;
+	  	     size++;
 		}
-		count++;
-
+ 		count++;
 	}
-
-	for (i = count - size; i < count; i++)
+	for (i = count - size; i count; i++)
 	{
 		oi = oi + ((*(s + i) - 48) * m);
-		m /= 10;
+		m /=10;
 	}
-		return (oi * pn);
+	return (oi * pn) ;
 }
+
+			
